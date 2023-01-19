e67314db641e5b6cf9181bdef6d228ec5dbcf872
Md Anzar Ahmad <anzarahmadcea@gmail.com>
2023-01-19 19:21:21 +0530

    edit App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 // import CodeSnippets from "./pages/CodeSnippets";
 import AutoCode from "./pages/AutoCode";
@@ -6,26 +6,17 @@ import ExplainCode from "./pages/ExplainCode";
 import TranslateCode from "./pages/TranslateCode";
 
 const App = () => (
-  // <BrowserRouter>
-  //   <Routes>
-  //     <Route path='/' element={<Home />} />
-  //     {/* <Route path='/codeSnippets' element={<CodeSnippets />} /> */}
-  //     <Route path='/autocode' element={<AutoCode />} />
-  //     <Route path='/explaincode' element={<ExplainCode />} />
-  //     <Route path='/translatecode' element={<TranslateCode />} />
-  //   </Routes>
-  // </BrowserRouter>
-
   <BrowserRouter>
     <Routes>
-      <Switch>
-        <Route path='/' element={<Home />} />
-        <Route path='/autocode' element={<AutoCode />} />
-        <Route path='/explaincode' element={<ExplainCode />} />
-        <Route path='/translatecode' element={<TranslateCode />} />
-        <Route path='*' render={() => <Redirect to='/' />} />
-      </Switch>
+      <Route path='/' element={<Home />} />
+      {/* <Route path='/codeSnippets' element={<CodeSnippets />} /> */}
+      <Route path='/autocode' element={<AutoCode />} />
+      <Route path='/explaincode' element={<ExplainCode />} />
+      <Route path='/translatecode' element={<TranslateCode />} />
     </Routes>
+    <Link to='/autocode'></Link>
+    <Link to='/explaincode'></Link>
+    <Link to='/translatecode'></Link>
   </BrowserRouter>
 );
 
