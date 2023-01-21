246745529ab6e40f2605fe98222f02cca24019cb
Md Anzar Ahmad <anzarahmadcea@gmail.com>
2023-01-21 19:27:47 +0530

    fix footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,10 @@ import styles from "../style";
 import { socialMedia } from "../constants";
 
 const Footer = () => (
-  <footer className={`${styles.flexCenter} flex-col bg-primary`}>
-    <div className='flex justify-between items-center md:flex-row flex-col px-3 py-2 border-t-[1px] border-t-text-gradient absolute bottom-2 w-[92vw]'>
+  <footer
+    className={`${styles.flexCenter} flex-col bg-primary absolute bottom-0 w-full`}
+  >
+    <div className='flex justify-between items-center md:flex-row flex-col px-3 py-2 border-t-[1px] border-t-text-gradient w-[92vw]'>
       <p className='font-poppins font-normal text-center text-[12px] leading-[25px] text-white'>
         Copyright Ⓒ 2022. All Rights Reserved. <br className='sm:hidden' />{" "}
         <span className='sm:inline-block hidden text-gradient font-medium text-[14px]'>
